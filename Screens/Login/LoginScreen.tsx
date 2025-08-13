6f5fc2aa88bf0c5f2440c634e562796d70324c3e
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2025-08-13 13:58:05 +0200

    wip: login

--- Screens/Login/LoginScreen.tsx
+++ Screens/Login/LoginScreen.tsx
@@ -1,16 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Alert, Button, Text, TextInput, View } from "react-native";
 import { styles } from "./LoginScreen.styles";
+import { sendCredentials } from "@/controllers/loginController";
 
 export default function LoginScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
-    if (email && password) {
+  const handleLogin = async () => {
+    console.log("Sending information");
+    console.log(email, password);
+    if (!email || !password) {
+      Alert.alert("Fyll i alla fält!");
+      return;
+    }
+
+    const result = await sendCredentials(email, password);
+
+    if (result) {
       Alert.alert("Inloggningen lyckades");
     } else {
       Alert.alert("Fel");
+      return;
     }
   };
 
