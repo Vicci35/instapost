e23d922ec15b17d420323c40259847a627276ee7
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2025-08-13 17:31:00 +0200

    .

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import LoginScreen from "@/Screens/Login/LoginScreen";
+import React from "react";
+import { SafeAreaView } from "react-native";
+
+export default function Page() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <LoginScreen />
+    </SafeAreaView>
+  );
+}
