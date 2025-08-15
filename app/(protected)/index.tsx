8e73edb921926368b228c8d78bb6675820fa09dd
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2025-08-16 00:33:24 +0200

    add typescript and fontawesome to package.json. change icons in tabs

--- app/(protected)/index.tsx
+++ app/(protected)/index.tsx
@@ -1,9 +1,9 @@
 import { View, Text } from "react-native";
 
-export default function Dashboard() {
+export default function Home() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Protected dashboard</Text>
+      <Text>Protected home</Text>
     </View>
   );
 }
--- app/(protected)/profile.tsx
+++ app/(protected)/profile.tsx
@@ -4,6 +4,7 @@ export default function Profile() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Protected profile</Text>
+      {/* Log out option here  */}
     </View>
   );
 }
