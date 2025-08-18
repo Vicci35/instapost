a11e867dbdf1389f51d83bc0a919d004b9b9d536
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2025-08-18 14:22:49 +0200

    add post tab in protected routes

---
+++ app/(protected)/(post)/Post.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text, StyleSheet } from "react-native";
+
+export default function Post() {
+  return (
+    <SafeAreaView>
+      <Text>New post</Text>
+    </SafeAreaView>
+  );
+}
