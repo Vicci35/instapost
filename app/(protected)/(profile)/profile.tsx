cbba5c338ee92b242fc0cbe5269705a752c5f143
Vicci35 <toja.1999@hotmail.com>
2025-08-26 14:34:12 +0200

    minor fix

--- app/(protected)/(profile)/profile.tsx
+++ app/(protected)/(profile)/profile.tsx
@@ -1,30 +1,17 @@
 // app/protected/profile.tsx
 import React from "react";
 
-import { SafeAreaView, TouchableOpacity, Text } from "react-native";
+import { SafeAreaView } from "react-native";
 import { useRouter } from "expo-router";
 import ProfileScreen from "@/Screens/ProfileScreen/ProfileScreen";
-import { handleLogout } from "@/controllers/logoutController";
 import { styles } from "@/styles/protectedStyles";
-import { Platform } from "react-native";
-
-import { useContext } from "react";
-import { UserContext } from "@/contexts/userContext";
 
 export default function Profile() {
   const router = useRouter();
-  const { user, logout } = useContext(UserContext);
 
   return (
     <SafeAreaView style={styles.container}>
       <ProfileScreen />
-
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => handleLogout(router, Platform.OS, logout)}
-      >
-        <Text style={styles.buttonText}>Log out</Text>
-      </TouchableOpacity>
     </SafeAreaView>
   );
 }
