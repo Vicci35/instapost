5723b4b3f301500808e1b13e1526f0c3c65e70e3
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2025-08-28 13:57:19 +0200

    .

--- app/(protected)/(post)/Post.tsx
+++ app/(protected)/(post)/Post.tsx
@@ -3,6 +3,7 @@ import { Text, View, StyleSheet, Button, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useState } from "react";
 import { FontAwesome } from "@expo/vector-icons";
+import { cameraStyles } from "@/styles/cameraStyles";
 
 export default function Post() {
   const [facing, setFacing] = useState<CameraType>("back");
@@ -10,7 +11,7 @@ export default function Post() {
 
   if (!permission) {
     return (
-      <View style={styles.container}>
+      <View style={cameraStyles.container}>
         <Text style={{ fontSize: 100 }}>
           This app is not allowed to use the camera
         </Text>
@@ -20,7 +21,7 @@ export default function Post() {
 
   if (!permission.granted) {
     return (
-      <View style={styles.container}>
+      <View style={cameraStyles.container}>
         <Text>Camera Test</Text>
         <FontAwesome name="camera" size={48} color={"black"} />
         <Text>Allow this app to use the camera</Text>
@@ -35,45 +36,30 @@ export default function Post() {
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <CameraView style={styles.camera} facing={facing}>
-          <View style={styles.buttonContainer}></View>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-            {/* Fontawesome icon  */}
-            <Text style={styles.text}>Flip Camera</Text>
+      <View style={cameraStyles.container}>
+        <CameraView style={cameraStyles.camera} facing={facing}>
+          <View style={cameraStyles.buttonContainer}></View>
+          {/* Add:
+              - Change to FontAwesome icons
+              - Flash functionality
+              - Take photo button
+              - Delete taken photo
+          */}
+          <TouchableOpacity
+            style={cameraStyles.button}
+            onPress={toggleCameraFacing}
+          >
+            <Text style={cameraStyles.text}>Flip Camera</Text>
           </TouchableOpacity>
-          {/* Take photo button */}
         </CameraView>
 
+        {/* 
+                Add:
+                Upload from phone gallery
+                Next button --> To edit post 
+            */}
         <Text> Or upload photo from gallery</Text>
       </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-  },
-  camera: {
-    width: 400,
-    height: 400,
-  },
-  buttonContainer: {
-    flex: 1,
-    flexDirection: "row",
-    backgroundColor: "transparent",
-    margin: 34,
-  },
-  button: {
-    flex: 1,
-    alignSelf: "center",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "white",
-  },
-});
