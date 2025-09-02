aeeae09fb76c0f5604f1132331d846a743733c2c
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2025-09-02 14:56:36 +0200

    wip: new post

--- Screens/ProfileScreen/editProfilScreen.tsx
+++ Screens/ProfileScreen/editProfilScreen.tsx
@@ -10,7 +10,6 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { useRouter } from "expo-router";
 import { useContext, useEffect, useState } from "react";
 import styles from "../../styles/editProfilStyles";
-import { Platform } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { updateProfile } from "@/controllers/userController";
 
@@ -24,6 +23,7 @@ export default function EditProfile() {
   const [name, setName] = useState("");
   const [bio, setBio] = useState("");
   const [profilePic, setProfilePic] = useState<string | null>(null);
+  const platform = Platform.OS;
 
   useEffect(() => {
     if (user) {
@@ -62,7 +62,13 @@ export default function EditProfile() {
     if (!token) throw new Error("Ingen token hittades. logga in igen");
 
     try {
-      const updatedUser = await updateProfile(token, name, bio, profilePic);
+      const updatedUser = await updateProfile(
+        token,
+        name,
+        bio,
+        profilePic,
+        platform
+      );
 
       if (!updatedUser) throw new Error("Misslyckades med att spara profil");
 
