5d7e0a6f744163e0e0873d7a8d2731378e60b5f1
VT <vithanh10@hotmail.com>
2025-09-05 17:08:24 +0200

    change to my ip adress

--- controllers/ImageController.tsx
+++ controllers/ImageController.tsx
@@ -31,7 +31,7 @@ export const handleNewPost = async (
   }
 
   const URL =
-    platform === "web" ? "http://localhost:3000" : "http://192.168.68.105:3000";
+    platform === "web" ? "http://localhost:3000" : "http://192.168.1.140:3000";
 
   const response = await fetch(URL + "/posts/new", {
     method: "POST",
