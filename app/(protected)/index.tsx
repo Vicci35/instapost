53f5b8c8e6bdcb0835226a205baa6d95e8155354
VT <vithanh10@hotmail.com>
2025-09-05 20:19:26 +0200

    profil image on the post

--- app/(protected)/index.tsx
+++ app/(protected)/index.tsx
@@ -54,7 +54,7 @@ export default function Home() {
   const BACKEND_URL =
     Platform.OS === "web"
       ? "http://localhost:3000"
-      : "http://192.168.68.104:3000";
+      : "http://192.168.1.140:3000";
 
   const currentUserId = user?._id;
 
